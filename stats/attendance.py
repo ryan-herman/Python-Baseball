d0b8281597297a0b5cc2eaefdb0e3a5837e55f2d
ryan-herman <47902796+ryan-herman@users.noreply.github.com>
2019-02-26 13:57:02 -0500

    Update attendance.py
    Changed mean line label to be titleized, linestyle to be be correctly dashed, and the games import from data

--- stats/attendance.py
+++ stats/attendance.py
@@ -1,7 +1,8 @@
 ##  IMPORTS 
 import pandas as pd
 import matplotlib.pyplot as plt 
-from data import games
+
+from .data import games
 
 ##  SELECT AND PLOT DATA
 ########################
@@ -18,5 +19,5 @@ attendance.loc[:, 'column'] = pd.to_numeric(attendance.loc[:, 'column'])
 attendance.plot(x='year', y='attendance', figsize=(15, 7), kind='bar')
 plt.xlabel('Year')  ##  x axis 
 plt.ylabel('Attendance')    ##  y axis
-plt.axhline(y = attendance['Year'].mean(), label='mean', linestyle = 'dashed', color = 'green')
+plt.axhline(y = attendance['Year'].mean(), label='Mean', linestyle = '--', color = 'green')
 plt.show() 
