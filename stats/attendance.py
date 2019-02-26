76ec3e38fb76d30f61cfcdd9e257d30c1557c5eb
ryan-herman <47902796+ryan-herman@users.noreply.github.com>
2019-02-26 13:40:46 -0500

    Update attendance.py
    Added code to select and plot attendance data

---
+++ stats/attendance.py
@@ -0,0 +1,22 @@
+##  IMPORTS 
+import pandas as pd
+import matplotlib.pyplot as plt 
+from data import games
+
+##  SELECT AND PLOT DATA
+########################
+## Select Attendance Data
+attendance = games.loc[(games['type'] == 'info') & (games['multi2'] == 'attendance'), ['year', 'multi3']]
+
+##  Column Labels
+attendance.columns = ['year', 'attendance']
+
+##  Convert to Numeric
+attendance.loc[:, 'column'] = pd.to_numeric(attendance.loc[:, 'column'])
+
+##  Plot DataFrame
+attendance.plot(x='year', y='attendance', figsize=(15, 7), kind='bar')
+plt.xlabel('Year')  ##  x axis 
+plt.ylabel('Attendance')    ##  y axis
+plt.axhline(y = attendance['Year'].mean(), label='mean', linestyle = 'dashed', color = 'green')
+plt.show() 
