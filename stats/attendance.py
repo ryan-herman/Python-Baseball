5c30bb799cc7931d206ee51329931f97c43c922b
ryan-herman <47902796+ryan-herman@users.noreply.github.com>
2019-03-08 14:14:48 -0500

    Attendance
    Implemented Attendance dataframe with year and attendance data.  Results are plotted and displayed

--- stats/attendance.py
+++ stats/attendance.py
@@ -1,23 +1,21 @@
-##  IMPORTS 
+##  IMPORTS
 import pandas as pd
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+from data import games
 
-from .data import games
-
-##  SELECT AND PLOT DATA
-########################
-## Select Attendance Data
+## Select Attendance
 attendance = games.loc[(games['type'] == 'info') & (games['multi2'] == 'attendance'), ['year', 'multi3']]
 
-##  Column Labels
+
+## New Column Labels
 attendance.columns = ['year', 'attendance']
 
-##  Convert to Numeric
-attendance.loc[:, 'column'] = pd.to_numeric(attendance.loc[:, 'column'])
+## Convert to Numeric
+attendance.loc[:, 'attendance'] = pd.to_numeric(attendance.loc[:, 'attendance'])
 
-##  Plot DataFrame
-attendance.plot(x='year', y='attendance', figsize=(15, 7), kind='bar')
-plt.xlabel('Year')  ##  x axis 
-plt.ylabel('Attendance')    ##  y axis
-plt.axhline(y = attendance['Year'].mean(), label='Mean', linestyle = '--', color = 'green')
-plt.show() 
+## Plot Attendance DataFrame
+attendance.plot(x='year', y='attendance', figsize=(15,7), kind='bar')
+plt.xlabel('Year') # x axis
+plt.ylabel('Attendance') # y axis
+plt.axhline(y = attendance['attendance'].mean(), label = 'Mean', linestyle='--', color = 'green')
+plt.show()
