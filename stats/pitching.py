35efc106f7bd31ab77491afdec8a72849e357a5b
ryan-herman <47902796+ryan-herman@users.noreply.github.com>
2019-03-08 15:13:16 -0500

    Update pitching.py
    Pitching data collected into data frame and plotted to scatter plot.

--- stats/pitching.py
+++ stats/pitching.py
@@ -1,2 +1,22 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from data import games
+
+## Plays DataFrame
+plays = games[games['type'] == 'play']
+
+## StrikeOuts DataFrame
+strike_outs = plays[plays['event'].str.contains('K')]
+
+## Group Data by Year and Game
+strike_outs = strike_outs.groupby(['year', 'game_id']).size()
+
+## Reset Index on Strike Outs
+strike_outs = strike_outs.reset_index(name='strike_outs')
+
+## Convert Year and Strike_Outs Columns to Numerics
+strike_outs = strike_outs.loc[:, ['year', 'strike_outs']].apply(pd.to_numeric)
+
+## Change Plot Formatting
+strike_outs.plot(x = 'year', y = 'strike_outs', kind = 'scatter').legend('Strike Outs')
+plt.show()
